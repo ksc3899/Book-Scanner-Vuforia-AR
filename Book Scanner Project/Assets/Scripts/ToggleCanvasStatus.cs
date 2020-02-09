0e4c42ac405e20a407878c3b3827a861b710cf0c
Sai Charan Kommana <saicharan.k3899@gmail.com>
2020-02-09 10:22:13 +0530

    Booker Scanner made in Vuforia with target for Narasimha book

---
+++ Book Scanner Project/Assets/Scripts/ToggleCanvasStatus.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleCanvasStatus : MonoBehaviour
+{
+    public GameObject markerNotFound;
+    public GameObject markerFound;
+    public GameObject scanner;
+
+    public void OnDetected()
+    {
+        StartCoroutine("TargetImageDetected");
+    }
+
+    public void OnLost()
+    {
+        StopCoroutine("TargetImageDetected");
+        scanner.SetActive(false);
+        markerFound.SetActive(false);
+        markerNotFound.SetActive(true);
+    }
+
+    private IEnumerator TargetImageDetected()
+    {
+        scanner.SetActive(true);
+        yield return new WaitForSeconds(2f);
+        scanner.SetActive(false);
+        markerNotFound.SetActive(false);
+        markerFound.SetActive(true);
+    }
+}
